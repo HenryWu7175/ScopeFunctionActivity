ca331f56d8febf0f0aaf20c9502b780c39a7bb0a
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-02-12 16:38:46 -0500

    changes made to make all functions single expressions. added test cases in the main to test via the logcat debugger

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -16,7 +16,25 @@ class MainActivity : AppCompatActivity() {
         // You can test your helper functions by  calling them from onCreate() and
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
+        var numbersarray = getTestDataArray()
+        Log.d("function output", numbersarray.toString())
 
+        val doublearray: MutableList<Double> = mutableListOf(1.0, 50.0, 1.0, 55.0, 100.0)
+        var trueifavgless = averageLessThanMedian(doublearray)
+        Log.d("function output", trueifavgless.toString())
+
+        val collection1 = listOf(10, 20, 30, 40, 50)
+        val position1 = 2 // Should display "30"
+        val view1 = getView(position1, null, collection1, this)
+        Log.d("function output", "View text (new): ${(view1 as TextView).text}")
+
+        val collection2 = listOf(100, 200, 300, 400, 500)
+        val position2 = 1 // Should display "200"
+        val recycledView = TextView(this).apply { text = "Old Value" }
+        val view2 = getView(position2, recycledView, collection2, this)
+
+        Log.d("function output", "View text (recycled): ${(view2 as TextView).text}")
+        Log.d("function output", "Is recycled view same? ${recycledView === view2}")
     }
 
 
@@ -27,39 +45,27 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
-    }
+    private fun getTestDataArray(): List<Int> =
+        MutableList(10) { Random.nextInt() }.apply { sort() }
 
-    // Return true if average value in list is greater than median value, false otherwise
-    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
 
-        return avg < median
-    }
-
-    // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
+    // Return true if average value in list is less than median value, false otherwise
+    private fun averageLessThanMedian(listOfNumbers: List<Double>) =
+        listOfNumbers.sorted().run {
+            val median = if (size % 2 == 0)
+                (this[size / 2] + this[(size - 1) / 2]) / 2
+            else
+                this[size / 2]
+            listOfNumbers.average() < median
         }
 
-        textView.text = collection[position].toString()
 
-        return textView
-    }
-
+    // Create a view from an item in a collection, but recycle if possible
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View =
+        (recycledView as? TextView ?: TextView(context).apply {
+            setPadding(5, 10, 10, 0)
+            textSize = 22f
+        }).apply {
+            text = collection[position].toString()
+        }
 }
